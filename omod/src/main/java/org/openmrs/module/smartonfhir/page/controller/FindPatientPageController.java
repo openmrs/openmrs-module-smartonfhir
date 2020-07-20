ef486922f9a81fe26e5d7a2a2e437ce75c4045fe
Siddharth Vaish <sidvaish97@gmail.com>
2020-07-21 02:41:09 +0530

    state param add and secret key

--- omod/src/main/java/org/openmrs/module/smartonfhir/page/controller/FindPatientPageController.java
+++ omod/src/main/java/org/openmrs/module/smartonfhir/page/controller/FindPatientPageController.java
@@ -28,9 +28,10 @@ public class FindPatientPageController {
 	 * @param sessionContext
 	 */
 	public void get(PageModel model, @RequestParam("app") AppDescriptor app, @RequestParam("access_token") String token,
-	        UiSessionContext sessionContext, UiUtils ui) {
-
-		model.addAttribute("afterSelectedUrl", app.getConfig().get("afterSelectedUrl").getTextValue() + "&token=" + token);
+	        @RequestParam("state") String state, UiSessionContext sessionContext, UiUtils ui) {
+		
+		model.addAttribute("afterSelectedUrl",
+		    app.getConfig().get("afterSelectedUrl").getTextValue() + "&token=" + token + "&state=" + state);
 		model.addAttribute("heading", app.getConfig().get("heading").getTextValue());
 		model.addAttribute("label", app.getConfig().get("label").getTextValue());
 		model.addAttribute("showLastViewedPatients", app.getConfig().get("showLastViewedPatients").getBooleanValue());
--- omod/src/main/java/org/openmrs/module/smartonfhir/web/servlet/SmartPatientSelected.java
+++ omod/src/main/java/org/openmrs/module/smartonfhir/web/servlet/SmartPatientSelected.java
@@ -9,6 +9,7 @@
  */
 package org.openmrs.module.smartonfhir.web.servlet;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,9 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.keycloak.crypto.JavaAlgorithm;
+import org.keycloak.crypto.KeyWrapper;
+import org.keycloak.crypto.MacSignatureSignerContext;
+import org.keycloak.crypto.SignatureSignerContext;
 import org.keycloak.jose.jws.JWSBuilder;
 import org.keycloak.representations.JsonWebToken;
 
@@ -26,7 +33,11 @@ public class SmartPatientSelected extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		String token = req.getParameter("token");
+		System.out.println("token " + token);
 		String patientId = req.getParameter("patientId");
+		System.out.println("patientId " + patientId);
+		String state = req.getParameter("state");
+		System.out.println("state " + state);
 		
 		if (token == null || patientId == null) {
 			// this simulates what the controller would do if required parameteres are missing
@@ -41,11 +52,25 @@ public class SmartPatientSelected extends HttpServlet {
 			String decodedValue = URLDecoder.decode(entry.getValue(), "UTF-8");
 			tokenSentBack.setOtherClaims(entry.getKey(), decodedValue);
 		}
-		String appToken = new JWSBuilder().jsonContent(tokenSentBack).none();
+		
+		// create token signer
+		String secretKey = "dbc3ad18-cce2-4793-a0d1-5fbbc733bd56";
+		SecretKeySpec hmacSecretKeySpec = new SecretKeySpec(secretKey.getBytes(),
+		        JavaAlgorithm.getJavaAlgorithm("HmacSHA256"));
+		KeyWrapper keyWrapper = new KeyWrapper();
+		keyWrapper.setAlgorithm("HmacSHA256");
+		keyWrapper.setSecretKey(hmacSecretKeySpec);
+		SignatureSignerContext signer = new MacSignatureSignerContext(keyWrapper);
+		
+		// sign and encode launch context token
+		String appToken = new JWSBuilder().jsonContent(tokenSentBack).sign(signer);//.hmac256(hmacSecretKeySpec);
 		String encodedToken = URLEncoder.encode(appToken, "UTF-8");
 		
 		String decodedUrl = URLDecoder.decode(token, "UTF-8");
-		System.out.println(decodedUrl.replace("{APP_TOKEN}", encodedToken));
-		res.sendRedirect(decodedUrl.replace("{APP_TOKEN}", encodedToken));
+		String finalToken = decodedUrl.replace("{APP_TOKEN}", encodedToken);
+		String[] tokenParts = finalToken.split("\\.");
+		System.out.println(new String(Base64.getDecoder().decode(tokenParts[1]), StandardCharsets.UTF_8));
+		//		System.out.println(decodedUrl.replace("{APP_TOKEN}", encodedToken));
+		res.sendRedirect("http://127.0.0.1:9090/?code=" + finalToken + "&state=" + state);
 	}
 }
