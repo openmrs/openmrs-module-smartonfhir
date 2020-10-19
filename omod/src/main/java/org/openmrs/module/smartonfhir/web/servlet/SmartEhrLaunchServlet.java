072a5b75c72e118adee942fa43c3008674e4621c
Siddharth Vaish <sidvaish97@gmail.com>
2020-10-19 18:20:19 +0530

    emr external app launch (#6)

---
+++ omod/src/main/java/org/openmrs/module/smartonfhir/web/servlet/SmartEhrLaunchServlet.java
@@ -0,0 +1,36 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.smartonfhir.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
+
+public class SmartEhrLaunchServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String patientId = req.getParameter("patientId");
+		String url = "http://127.0.0.1:9090/launch-standalone.html?iss=http://localhost:8080/openmrs/ws/fhir2/R4&launch="
+		        + patientId;
+		
+		if (StringUtils.isBlank(url)) {
+			resp.sendError(HttpStatus.SC_BAD_REQUEST, "A url must be provided");
+			return;
+		}
+		
+		resp.sendRedirect(resp.encodeRedirectURL(url));
+	}
+}
