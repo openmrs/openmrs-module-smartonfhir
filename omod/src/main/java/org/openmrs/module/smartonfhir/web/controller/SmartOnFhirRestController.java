2d49b8f79766816a7751eadfe1a665fc2e95e25f
theanandankit <ankitanand2909@gmail.com>
2021-02-09 17:22:55 +0530

    Add Smart Access Servlet

---
+++ omod/src/main/java/org/openmrs/module/smartonfhir/web/controller/SmartOnFhirRestController.java
@@ -0,0 +1,66 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.smartonfhir.web.controller;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.keycloak.crypto.Algorithm;
+import org.keycloak.crypto.JavaAlgorithm;
+import org.keycloak.crypto.KeyWrapper;
+import org.keycloak.crypto.MacSignatureSignerContext;
+import org.keycloak.crypto.SignatureSignerContext;
+import org.keycloak.jose.jws.JWSBuilder;
+import org.keycloak.representations.JsonWebToken;
+import org.openmrs.module.smartonfhir.util.SmartSecretKeyHolder;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/rest/" + RestConstants.VERSION_1 + SmartOnFhirRestController.SMARTONFHIR_NAMESPACE)
+public class SmartOnFhirRestController extends BaseRestController {
+	
+	public static final String SMARTONFHIR_NAMESPACE = "/smartonfhir";
+	
+	@RequestMapping(value = "/accessConformation", method = RequestMethod.GET)
+	public void accessConformation(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		String token = request.getParameter("token");
+		
+		if (token == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		JsonWebToken tokenSendBack = new JsonWebToken();
+		tokenSendBack.setOtherClaims("username", "admin");
+		
+		SecretKeySpec hmacSecretKeySpec = new SecretKeySpec(SmartSecretKeyHolder.getSecretKey(), JavaAlgorithm.HS256);
+		KeyWrapper keyWrapper = new KeyWrapper();
+		keyWrapper.setAlgorithm(Algorithm.HS256);
+		keyWrapper.setSecretKey(hmacSecretKeySpec);
+		SignatureSignerContext signer = new MacSignatureSignerContext(keyWrapper);
+		
+		String appToken = new JWSBuilder().jsonContent(tokenSendBack).sign(signer);
+		String encodedToken = URLEncoder.encode(appToken, StandardCharsets.UTF_8.name());
+		
+		String decoded = URLDecoder.decode(token, StandardCharsets.UTF_8.name());
+		response.sendRedirect(decoded.replace("{APP_TOKEN}", encodedToken));
+	}
+}
