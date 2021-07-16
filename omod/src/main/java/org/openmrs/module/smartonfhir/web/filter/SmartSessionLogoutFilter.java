d15fedbf28c53d19778d80b54cc497e7215de364
Ankit kumar <49350053+theanandankit@users.noreply.github.com>
2021-07-16 23:01:03 +0530

    FM2-202: Expire the OAuth2 token when the user logs out of OpenMRS (#20)

---
+++ omod/src/main/java/org/openmrs/module/smartonfhir/web/filter/SmartSessionLogoutFilter.java
@@ -0,0 +1,75 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.smartonfhir.web.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.openmrs.module.smartonfhir.util.KeycloakConfigHolder;
+
+@Slf4j
+public class SmartSessionLogoutFilter implements Filter {
+	
+	private String logoutUrl = null;
+	
+	@Override
+	public void init(FilterConfig filterConfig) {
+		logoutUrl = KeycloakDeploymentBuilder.build(KeycloakConfigHolder.getKeycloakConfig()).getLogoutUrl().toTemplate();
+	}
+	
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+	        throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		if (request.getRequestURI().contains("/logout")) {
+			keycloakSessionLogout();
+		}
+		
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+	
+	private void keycloakSessionLogout() throws IOException {
+		CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().disableRedirectHandling().build();
+		CloseableHttpResponse closeableHttpResponse = null;
+		try {
+			closeableHttpResponse = closeableHttpClient.execute(new HttpGet(logoutUrl));
+		}
+		finally {
+			if (closeableHttpResponse != null) {
+				try {
+					closeableHttpResponse.close();
+				}
+				finally {
+					closeableHttpClient.close();
+				}
+			} else {
+				closeableHttpClient.close();
+			}
+		}
+	}
+}
